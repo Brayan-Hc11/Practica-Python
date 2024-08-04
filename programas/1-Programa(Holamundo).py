2700e9716bd3d173485ff54c862897ea0d183658
Brayan-Hc11 <brayanhernandezca11@gmail.com>
2024-08-04 18:37:04 -0500

    Se creo el primer programa de python adicional a eso se creo la documentación adecuada para este programa.

--- programas/1-Programa(Holamundo).py
+++ programas/1-Programa(Holamundo).py
@@ -1,1 +1,3 @@
-print("Hola mundo desde python")
+#Print("") en Python es una función incorporada que se utiliza para mostrar información en la consola, Es una de las funciones más comunes 
+#y se usa para mostrar texto, resultados de calculo, valores de variables y más.
+print('Hola mundo desde python')
