512cb8e407a0fc23d04d42b0fb1e887659180624
Brayan-Hc11 <brayanhernandezca11@gmail.com>
2024-08-15 20:49:25 -0500

    Se ha añadido un nuevo directorio en donde se desarrollara un curso de python adicionando la IA, además se ha terminado el episodio que hablaba de los strings.

--- programas/3-Programa(Strings).py
+++ programas/3-Programa(Strings).py
@@ -27,9 +27,35 @@ print(mensaje2 + espacio + nombre)
 val_uno = 1
 val_dos = 2
 resultado = val_uno + val_dos
-
 #Conversion de Numeros a Cadenas
 resultado = str(resultado)
-
 #Ejecución
 print('El resultado de la suma es: ' + resultado)
+
+
+#Busqueda: Consiste en localizar dentro de una cadena, una subcadena más pequeña a un caracter. Para lo cual es necesario utilizar el método FIND
+# Variables
+mensaje3 = 'Hola Mundo!'
+buscar_subcadena = mensaje3.find('Mundo!')
+conversion = str(buscar_subcadena)
+
+#Ejecución 
+print('Este es el resultado de la busqueda:' + conversion)
+
+#Extracción: Se trata de sacar fuera de una cadena, una porción de la misma según su posición dentro de ella. Para ello es necesario indicar la posición a extraer [1:5]
+#Variables 
+mensaje4 = 'Hola mundo!'
+extraer_cadena = mensaje4[5:10]
+
+#Ejecución 
+print('Este es el resultado de la extración: ' + extraer_cadena)
+
+#Comparación: Se utiliza para comparar dos cadenas de caracteres. para ello se utiliza el operador (==)
+comparacion_1 = 'Hola mundo!'
+comparacion_2 = 'Hola mundo!'
+operacion = comparacion_1 == comparacion_2
+conversion_ = str(operacion)
+
+#Ejecución 
+print('El resultado de la comparción es: ' + conversion_)
+
