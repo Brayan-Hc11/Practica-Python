7bad6b48ee13d87161753fc04e4dacbbfcafe01e
Brayan-Hc11 <brayanhernandezca11@gmail.com>
2024-08-19 12:16:14 -0500

    Se añadio el capitulo de las palabras reservadas en python.

---
+++ programas/4-Programas(Palabras resservadas).py
@@ -0,0 +1,17 @@
+#Palbras reservadas: son indentificadores para uso exclusivo del lenguaje de progrmación, que no pueden ser utilizadas para indentificar y nombrar 
+#variables, métodos, objtos  o cualquier elemento dentro de nuestro código.
+
+#Al intentar utilizar alguna de estas palabras reservadas fuera del uso que tienen asignado, lo que obtendremos será un error de compilación y nuestro programa
+# no podrá ser ejecutado.
+
+#Conjunto de palabras reservadas en Python (and, del ,for ,is , raise, assert, if, else, elif, from, lambda, return, break, global, not, try, class, except, or, while
+#, continue, exec import, yield, del, finally, in, print.)
+
+#Ejemplo 
+
+print = 6 
+Print = 4
+
+resultado = print + Print
+
+print(resultado)
